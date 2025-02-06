3721912623dfc0eaf9b51b0391a8eb813c30de70
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2025-02-06 11:04:00 +0100

    changed dockerfile frontend

--- mongo-init.js
+++ mongo-init.js
@@ -4,15 +4,31 @@ db = db.getSiblingDB("candidates-app");
 db.createCollection("candidates"); 
 db.candidates.insertMany([
 	{
-		name: "John Doe",
-		email: "john.doe@example.com",
-		position: "Frontend Developer",
-		skills: ["JavaScript", "Angular", "TypeScript"]
+		name: "John",
+		surname: "Doe",
+		seniority: "junior",
+		years: 3,
+		availability: true
 	},
 	{
-		name: "Jane Smith",
-		email: "jane.smith@example.com",
-		position: "Backend Developer",
-		skills: ["Java", "Spring Boot", "MongoDB"]
+		name: "Jane",
+		surname: "Smith",
+		seniority: "junior",
+		years: 1,
+		availability: false
+	},
+	{
+		name: "Marta",
+		surname: "Perez",
+		seniority: "senior",
+		years: 10,
+		availability: true
+	},
+	{
+		name: "Javier",
+		surname: "Cervantez",
+		seniority: "junior",
+		years: 2,
+		availability: true
 	}
 ]);
